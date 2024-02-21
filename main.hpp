529718384a64b98cd99fcc0b252380cbfdbec4a8
Skylar Au-Juarez <84100709+SkylarAJ@users.noreply.github.com>
2024-02-20 20:35:44 -0800

    main.hpp

--- main.hpp
+++ main.hpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <cstring>
 #include <sstream>
+#include <vector>
+
 using namespace std;
 struct Name
 {
@@ -33,7 +35,43 @@ void sortPerson(vector<Person> &);
 
 void makePerson(vector<Person> &person)
 {
+    ifstream file("people-100.csv");
+    if (!file.is_open())
+    {
+        cerr << "Error opening file." << endl;
+        return;
+    }
+
+    string line;
+    while (getline(file, line))
+    {
+        stringstream ss(line);
+        Person p;
+        string token;
+
+        // Read and assign each field of the Person struct
+        getline(ss, token, ',');
+        p.no = stoi(token);
+        getline(ss, p.ID, ',');
+        getline(ss, p.name.first, ',');
+        getline(ss, p.name.last, ',');
+        getline(ss, p.email, ',');
+        getline(ss, p.gender, ',');
+        getline(ss, p.phone, ',');
+        getline(ss, token, '-');
+        p.bdate.year = stoi(token);
+        getline(ss, token, '-');
+        p.bdate.month = stoi(token);
+        getline(ss, token, ',');
+        p.bdate.day = stoi(token);
+        getline(ss, p.jobtitle, ',');
+
+        person.push_back(p);
+    }
+
+    file.close();
 }
+
 void printPerson(vector<Person> person)
 {
     vector<Person>::iterator it;
@@ -52,9 +90,21 @@ void printPerson(vector<Person> person)
     }
     cout << "**************************************************\n";
 }
+
 void sortPerson(vector<Person> &person)
 {
+    for (size_t i = 0; i < person.size() - 1; ++i)
+    {
+        for (size_t j = 0; j < person.size() - 1 - i; ++j)
+        {
+            if (person[j].name.last > person[j + 1].name.last)
+            {
+                swap(person[j], person[j + 1]);
+            }
+        }
+    }
 }
+
 /*******************************
  * Code your program here
  *******************************/
